9f14d5fd144c166ebfbd8a4231b2e3b1126bf2f3
Def <deferenseo@gmail.com>
2022-09-06 15:40:16 +0300

    adding input.value to taskList

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
 const form = document.querySelector('#form');
 const taskInput = document.querySelector('#taskInput');
+const tasksList = document.querySelector('#tasksList');
 
-console.log(taskInput);
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const taskText = taskInput.value;
+
+    const taskHTML = `
+    <li class="list-group-item d-flex justify-content-between task-item">
+        <span class="task-title">${taskText}</span>
+        <div class="task-item__buttons">
+            <button type="button" data-action="done" class="btn-action">
+                <img src="./img/tick.svg" alt="Done" width="18" height="18">
+            </button>
+            <button type="button" data-action="delete" class="btn-action">
+                <img src="./img/cross.svg" alt="Done" width="18" height="18">
+            </button>
+        </div>
+    </li>
+    `;
+    tasksList.insertAdjacentHTML('beforeend', taskHTML);
+    taskInput.value = '';
+    taskInput.focus();
+});
