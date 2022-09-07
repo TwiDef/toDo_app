aa398f2d70e1a4f6fa97c0db941e64a4cb9cbfce
Def <deferenseo@gmail.com>
2022-09-07 11:58:51 +0300

    added opportunity save/load tasks to/from localStorage

--- js/main.js
+++ js/main.js
@@ -4,6 +4,30 @@ const tasksList = document.querySelector('#tasksList');
 const emptyList = document.querySelector('#emptyList');
 
 let tasks = [];
+
+if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+}
+
+tasks.forEach(function(task) {
+    const cssClass = task.done ? "task-title task-title--done" : "task-title";
+
+    const taskHTML = `
+    <li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
+        <span class="${cssClass}">${task.text}</span>
+        <div class="task-item__buttons">
+            <button type="button" data-action="done" class="btn-action">
+                <img src="./img/tick.svg" alt="Done" width="18" height="18">
+            </button>
+            <button type="button" data-action="delete" class="btn-action">
+                <img src="./img/cross.svg" alt="Done" width="18" height="18">
+            </button>
+        </div>
+    </li>
+    `;
+    tasksList.insertAdjacentHTML('beforeend', taskHTML);
+});
+
 checkEmptyList();
 
 
@@ -23,6 +47,7 @@ function addTask(e) {
         done: false
     };
     tasks.push(newTask);
+    saveToLocalStorage();
 
     const cssClass = newTask.done ? "task-title task-title--done" : "task-title";
 
@@ -44,6 +69,7 @@ function addTask(e) {
     taskInput.focus();
 
     checkEmptyList();
+
 }
 
 function deleteTask(e) {
@@ -58,6 +84,8 @@ function deleteTask(e) {
     tasks.splice(index, 1);
 
     tasks = tasks.filter((task) => task.id !== id);
+    saveToLocalStorage();
+
     parentNode.remove();
 
     checkEmptyList();
@@ -71,6 +99,7 @@ function doneTask(e) {
 
     const task = tasks.find((task) => task.id === id);
     task.done = !task.done;
+    saveToLocalStorage();
 
     const taskTitle = parentNode.querySelector('.task-title');
     taskTitle.classList.toggle('task-title--done');
@@ -80,7 +109,7 @@ function checkEmptyList() {
     if (tasks.length === 0) {
         const emptyListHTML = `
         <li id="emptyList" class="list-group-item empty-list">
-            <img src="./img/leaf.svg" alt="Empty" width="48" class="mt-3">
+            <img src="./img/book.png" alt="Empty" width="48" class="mt-3">
             <div class="empty-list__title">Список дел пуст</div>
         </li>
         `;
@@ -91,4 +120,8 @@ function checkEmptyList() {
         const emptyListEl = document.querySelector('#emptyList');
         emptyListEl ? emptyListEl.remove() : null;
     }
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
